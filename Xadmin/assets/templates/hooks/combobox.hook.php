5036f72b7c9218e8ee29beff3073d659f05125d0
alooze <a.looze@gmail.com>
2014-02-19 20:21:34 +0200

    Изменена логика хуков select

--- Xadmin/assets/templates/hooks/combobox.hook.php
+++ Xadmin/assets/templates/hooks/combobox.hook.php
@@ -5,19 +5,21 @@
  *
  * Задача - заполнить плейсхолдер [+data+] опциями списка select
  * Данные для списка должны быть в конфиге в виде 
- * "list": {"value":"parent", "text": "category"} (не реализовано!)
+ * "list": "val1==name1|val2==name2..."
  *
  * либо
- * "list": "@SNIPPET НАЗВАНИЕ СНИППЕТА"
+ * "list": "@SNIPPET НАЗВАНИЕ СНИППЕТА" (не реализовано)
  *
  * либо
- * "list": "@SELECT id,category FROM TABLE_NAME WHERE CONDITION"
+ * --- "list": "@SELECT id,category FROM TABLE_NAME WHERE CONDITION" (не реализовано)
+ * +++ "list": "@SELECT id,category"
  *
  * @param $hookName - Название события (OnBeforeXadminInputTemplateRender)
  * @param $params - Массив с данными из конфига 
  * @param &$ret - ссылка на массив с плейсхолдерами
  */
 global $modx;
+$_ARM = $params['ARM'];
 if (!isset($params['list'])) return $ret;
 
 if (is_array($params['list'])) {
@@ -29,13 +31,23 @@ if (is_array($params['list'])) {
 }
 
 if (substr($params['list'], 0, 7) == '@SELECT') {
-    // пока не умничаем с AR, а просто делаем запрос
     $q = substr($params['list'], 8);
     $flds = substr($q, 0, strpos($q, ' '));
-    $fldsAr = explode(',', $flds);
-    $res = $modx->db->query('SELECT '.$q);
-    while ($row = $modx->db->getRow($res)) {
-        $ret['data'].= '<option value="'.$row[$fldsAr[0]].'">'.$row[$fldsAr[1]].'</option>';
+    list($k, $v) = explode(',', $flds);
+
+    $obj = new $_ARM();
+
+    $res = $obj::find('all');
+    foreach ($res as $row) {
+        $ret['data'].= '<option value="'.$row->$k.'">'.$row->$v.'</option>';
+    }    
+} else {
+    $arr = explode('|', $params['list']);
+    foreach ($arr as $row) {
+        list($key, $val) = explode('==', $row);
+        if (!isset($val) || $val == '') $val = $key;
+        $ret['data'].= '<option value="'.$key.'">'.$val.'</option>';
     }
-    return $ret;
 }
+
+return $ret;
--- Xadmin/assets/templates/hooks/select.hook.php
+++ Xadmin/assets/templates/hooks/select.hook.php
@@ -1,23 +1,25 @@
 <?php
 /**
- * combobox.hook.php
- * Хук-функция для обработки плейсхолдеров в поле combobox внешней админки
+ * select.hook.php
+ * Хук-функция для обработки плейсхолдеров в поле select внешней админки
  *
  * Задача - заполнить плейсхолдер [+data+] опциями списка select
  * Данные для списка должны быть в конфиге в виде 
- * "list": {"value":"parent", "text": "category"} (не реализовано!)
+ * "list": "val1==name1|val2==name2..."
  *
  * либо
- * "list": "@SNIPPET НАЗВАНИЕ СНИППЕТА"
+ * "list": "@SNIPPET НАЗВАНИЕ СНИППЕТА" (не реализовано)
  *
  * либо
- * "list": "@SELECT id,category FROM TABLE_NAME WHERE CONDITION"
+ * --- "list": "@SELECT id,category FROM TABLE_NAME WHERE CONDITION" (не реализовано)
+ * +++ "list": "@SELECT id,category"
  *
  * @param $hookName - Название события (OnBeforeXadminInputTemplateRender)
  * @param $params - Массив с данными из конфига 
  * @param &$ret - ссылка на массив с плейсхолдерами
  */
 global $modx;
+$_ARM = $params['ARM'];
 if (!isset($params['list'])) return $ret;
 
 if (is_array($params['list'])) {
@@ -29,13 +31,23 @@ if (is_array($params['list'])) {
 }
 
 if (substr($params['list'], 0, 7) == '@SELECT') {
-    // пока не умничаем с AR, а просто делаем запрос
     $q = substr($params['list'], 8);
-    $arr = explode(",", $q);
-    foreach ($arr as $key => $value) {
-        $flds = explode(":", $value);
-        $ret['data'].= '<option value="'.$flds[0].'">'.$flds[1].'</option>';
+    $flds = substr($q, 0, strpos($q, ' '));
+    list($k, $v) = explode(',', $flds);
+
+    $obj = new $_ARM();
+
+    $res = $obj::find('all');
+    foreach ($res as $row) {
+        $ret['data'].= '<option value="'.$row->$k.'">'.$row->$v.'</option>';
     }
+    // пока не умничаем с AR, а просто делаем запрос
+    // $q = substr($params['list'], 8);
+    // $arr = explode(",", $q);
+    // foreach ($arr as $key => $value) {
+    //     $flds = explode(":", $value);
+    //     $ret['data'].= '<option value="'.$flds[0].'">'.$flds[1].'</option>';
+    // }
     /*
     $flds = substr($q, 0, strpos($q, ' '));
     $fldsAr = explode(',', $flds);
@@ -44,5 +56,14 @@ if (substr($params['list'], 0, 7) == '@SELECT') {
         $ret['data'].= '<option value="'.$row[$fldsAr[0]].'">'.$row[$fldsAr[1]].'</option>';
     }
     */
-    return $ret;
+    
+} else {
+    $arr = explode('|', $params['list']);
+    foreach ($arr as $row) {
+        list($key, $val) = explode('==', $row);
+        if (!isset($val) || $val == '') $val = $key;
+        $ret['data'].= '<option value="'.$key.'">'.$val.'</option>';
+    }
 }
+
+return $ret;

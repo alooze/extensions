269d81d8f98fa41fda7dcaedb460567749449dc7
alooze <a.looze@gmail.com>
2014-03-10 13:49:26 +0200

    fix ajax form working

--- Xadmin/assets/templates/formouter.xadmin.php
+++ Xadmin/assets/templates/formouter.xadmin.php
@@ -1,25 +1,15 @@
-    <!-- <div id="dlg1" style="[+style+]"> -->
-        <div class="ftitle">[+title+]</div>
-        <form id="fm" method="post">
-            [+finner+]
-            <button type="button" class="btn btn-success" onclick="saveRow()">
-                Сохранить
-            </button>
-
-            <button type="button" class="btn btn-warning" onclick="$('#dlg').dialog('close')">
-                Отмена
-            </button>            
-            
-        </form>
-    <!-- </div> -->
-    <!-- <div id="dlg-buttons"> -->
-        <!-- <a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-ok" onclick="saveRow()">
+    <div class="ftitle">[+title+]</div>
+    <form id="fm" method="post">
+        [+finner+]
+        <button type="button" class="btn btn-success" onclick="saveRow()">
             Сохранить
-        </a>
-        <a href="javascript:void(0)" class="easyui-linkbutton" iconCls="icon-cancel" onclick="$('#dlg').dialog('close')">
+        </button>
+
+        <button type="button" class="btn btn-warning" onclick="$('#dlg').dialog('close')">
             Отмена
-        </a> -->
-    <!-- </div> -->
+        </button>            
+        
+    </form>
 <script>
 $(function() {
     // alert(rowData.p_category);
